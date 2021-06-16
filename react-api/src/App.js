4c5626f66e2dbd2846ed13560b1ab2737bb99446
Edward Riley <55459510+EDataScriptware@users.noreply.github.com>
2021-06-15 21:47:46 -0500

    ReactJS view employee works

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,22 +1,47 @@
-import React, {useEffect, useState} from 'react'
-import './App.css'
-import axios from 'axios';
+import React, {Component} from 'react'
 
-function Employees(){
-  const url = 'http://127.0.0.1:5000/employees'
-  const [employee, setEmployee] = useState(null)
-  
-  useEffect(() => {
-    axios.get(url)
-    .then(response => {console.log(response.data)})
-    .then(response => { 
-      setEmployee(response)
-    })
-  }, [url])
+class App extends Component 
+{
+  state =
+  {
+    data : [],
+    isLoaded : false
+  }
 
-  return(
-    <div></div>
-  )
+  async componentDidMount()
+  {
+  try 
+  {
+    const response = await fetch('http://127.0.0.1:5000/employees')
+    const data = await response.json()
+    console.log(data)
+    setTimeout(() => {
+      this.setState({ data: data.result, isLoaded : true})
+    }, 3000)
+
+  }
+  catch(err)
+  {
+    console.log(err)
+  }
+}
+
+  render(){
+    console.log('render method called')
+    const {data, isLoaded} = this.state;
+    return (
+    <div>
+      {!isLoaded ? <div>Loading...</div> :
+        <ul>
+        {data.map((item) => {
+          return <li key={item.emp_id}>{item.first_name} {item.last_name}</li>
+        })}
+        </ul>      
+      }
+    </div>
+    )
+  }
 
 }
-export default Employees
+
+export default App
